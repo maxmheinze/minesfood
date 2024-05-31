b0701266f8696d49b85e3faad0d6f7357649d223
Nikolas Kuschnig <nkuschnig@disroot.org>
2024-05-31 13:49:42 +0200

    Consider all upstream directions

--- code/20_basins.R
+++ code/20_basins.R
@@ -9,7 +9,7 @@ mines <- st_read("data/mines/global_mining_polygons_v2.gpkg")
 m <- mines |> filter(ISO3_CODE == "TZA") |> st_centroid()
 
 # https://data.hydrosheds.org/file/hydrobasins/standard/hybas_af_lev01-12_v1c.zip
-lvl <- "08"
+lvl <- "12"
 s <- st_read(paste0("data/hybas_af_lev01-12_v1c/hybas_af_lev", lvl, "_v1c.shp"))
 s <- st_make_valid(s)
 d <- s |> select(HYBAS_ID, NEXT_DOWN) |> st_drop_geometry()
@@ -24,15 +24,14 @@ stream <- \(id, n = 1L, max = 11L, down = TRUE) {
   if(down) {
     id_next <- d[["NEXT_DOWN"]][d[["HYBAS_ID"]] == id]
   } else { # Upstream
-    id_next <- d[["HYBAS_ID"]][d[["NEXT_DOWN"]] == id]
+    id_next <- d[["HYBAS_ID"]][d[["NEXT_DOWN"]] %in% id]
   }
   if(all(id_next == 0) || length(id_next) == 0) return()
-  return(c(id_next, Recall(id_next[1L], n = n + 1L)))
+  return(c(id_next, Recall(id_next, n = n + 1L)))
 }
-downstream_ids <- sapply(treated_id, stream, down = TRUE)
-upstream_ids <- sapply(treated_id, stream, down = FALSE)
+downstream_ids <- lapply(treated_id, stream, down = TRUE)
+upstream_ids <- lapply(treated_id, stream, down = FALSE)
 # Todo:
-#   There may be multiple upstreams, only the first one ([1L]) is taken
 #   We should probably at least track the order of a basin
 
 s <- s |> mutate(
@@ -41,12 +40,13 @@ s <- s |> mutate(
       ifelse(HYBAS_ID %in% unlist(upstream_ids), "upstream",
         NA_character_)))
   )
+s |> pull(status) |> table()
 
 # Plot areas
 t <- st_crop(s, st_bbox(s |> filter(!is.na(status)))) |>
   tm_shape() +
-  tm_fill("status") +
+  tm_fill("status", colorNA = "transparent") +
   tm_borders() +
   tm_shape(m) +
-  tm_dots(col = "black", border.col = "white", size = .5, shape = 23)
-tmap_save(t, "basins_mines.png")
+  tm_dots(col = "black", border.col = "white", size = .3, shape = 23)
+tmap_save(t, paste0("basins_mines-l", lvl, ".png"))
