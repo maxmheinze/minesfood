965c8d8024cfb476de2952ed2c01f3a48e870ca5
oDNAuio <lukas.vashold@gmx.at>
2024-07-03 15:50:36 +0000

    initialised ESA cropland mask for time-varying areas of EVI computation

---
+++ code/23_ESA_cropland_mask.R
@@ -0,0 +1,39 @@
+# Load Packages -----------------------------------------------------------
+
+rm(list = ls())
+
+pacman::p_load(
+  sf,
+  dplyr,
+  tmap,
+  tidyverse,
+  countrycode,
+  geosphere, 
+  stars, 
+  terra,
+  ncdf4
+)
+
+# basins to get data for
+basins <- read_sf("/data/jde/processed/relevant_basins.gpkg")
+
+africa <- st_bbox(rnaturalearth::ne_countries(continent = "africa"))
+
+files <- list.files("/data/redd/land_use/")
+years <- 2000:2022
+
+for(i in years) {
+  ff <- files[grepl(i, files)]
+  lc_data <- rast(paste0("/data/redd/land_use/", ff), 
+                  subds = "lccs_class")
+  lc_data <- crop(lc_data, africa) # crop to extent of basins
+  lc_data <- subst(lc_data, c(10, 20), 1, NA)
+  # lc_data <- app(lc_data, function(x) {ifelse(x %in% c(10, 20, 30), 
+  #                                             1, NA)})
+  writeRaster(lc_data, 
+              filename = paste0("/data/jde/processed/land_use/croplands_", i, ".tif"), 
+              overwrite = TRUE)
+  rm(lc_data); gc()
+  cat("Year ", i, " done!\n")
+}
+
