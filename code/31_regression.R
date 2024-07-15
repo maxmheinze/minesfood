b781070a1d76dd3ece3ad6560c75064caf027b8c
oDNAuio <lukas.vashold@gmx.at>
2024-07-15 16:03:31 +0000

    split up merge of data and regressions

--- code/31_regression.R
+++ code/31_regression.R
@@ -4,75 +4,17 @@
 rm(list = ls())
 
 pacman::p_load(
-  tidyverse,
+  dplyr,
   rddtools, 
   countrycode,
-  magrittr, 
-  terra, 
-  countrycode, 
-  haven, 
-  rnaturalearth, 
-  rnaturalearthdata, 
-  sf, 
-  readxl, 
-  fixest, 
-  rdrobust
+  readr, 
+  fixest
 )
 
 
 # Read in Data ------------------------------------------------------------
+df_reg <- readRDS("/data/jde/processed/df_reg.RDS")
 
-# outcome
-basin_evi <- read_csv("/data/jde/basins_evi/basin_evi.csv")
-
-# treatment
-dup <- read_csv("/data/jde/processed/downstream_upstream_distance_ordered.csv")
-
-# geographical controls
-geo_controls <- readRDS("/data/jde/processed/geo_data_agg.RDS")
-
-# meteorological controls
-met_controls <- readRDS("/data/jde/processed/met_data_agg.RDS")
-
-
-# check whether some basins are up- and downstream at the same time
-dup |> 
-  group_by(HYBAS_ID, status) |> arrange(distance) |> slice_head(n = 1) |> 
-  group_by(HYBAS_ID) |> count() |> filter(n > 1)
-
-
-# Prepare Data for Regression ---------------------------------------------
-
-df_reg <- full_join(dup, basin_evi, by = "HYBAS_ID") |> 
-  left_join(geo_controls, by = "HYBAS_ID") |> 
-  left_join(met_controls, by = c("HYBAS_ID", "year")) |> 
-  mutate(t.trend = year - 2000)
-
-# what's done here is that we exclude basins where some of the time-invariant stuff
-# is missing and that we assign a downstream basin to be unique for a mine? Meaning
-# that a basin can only be downstream to one mine but not more? we only lose ~50 
-# observations with that, would have expected more given that mines are clustered
-
-# Do we properly account for the possibility that a basin that is downstream to a 
-# mine should not be upstream to another?
-df_reg <- df_reg %>%
-  filter(!is.na(eco_id), year < 2024) |> 
-  mutate(downstream = ifelse(distance == 0, 1, downstream), 
-         distance = distance / 10^3) %>%
-  group_by(HYBAS_ID, year) %>%
-  arrange(distance) %>% 
-  slice_head(n = 1) %>%
-  ungroup()
-
-df_reg |> group_by(order, status) |> 
-  summarise(distance = mean(distance, na.rm = T)) |> 
-  arrange(status, order) |> print(n = 30)
-# distance is balanced by order, will take order to restrict number of basins
-
-# throw out basins where max_EVI is missing
-basins_unbalanced <- df_reg |> group_by(HYBAS_ID) |> count() |> filter(n < 23) |> pull(HYBAS_ID)
-
-df_reg <- df_reg |> filter(!HYBAS_ID %in% basins_unbalanced)
 
 # Downstream Regression, full sample -------------------------------------------
 
