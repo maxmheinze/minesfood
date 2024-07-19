b5d03202de3156299ad77cf75bcd7024ded204cc
gustavpirich <gustav@pirich.at>
2024-07-19 14:53:05 +0000

    update

--- code/22_prep_basins_ndvi.R
+++ code/22_prep_basins_ndvi.R
@@ -11,7 +11,7 @@ sapply(list.files("R", ".R$"), \(f) {source(paste0("R/", f)); TRUE})
 
 
 # Mining Polygons ---------------------------------------------------------
-evi_basins_1 <- read_csv(p("basins_evi/02_mean_africover_cropland_evi_basin.csv"))
+evi_basins_1 <- read_csv(p("basins_evi/01_mean_evi_basin.csv"))
 
 # preparing the EVI dataset
 max_evi_basins_1 <- evi_basins_1 %>%
@@ -25,81 +25,109 @@ max_evi_basins_1 <- evi_basins_1 %>%
   rename(max_EVI = mean_EVI,
          max_EVI_date = image_date)
 
-min_evi_basins <- evi_basins_2 %>%
-  dplyr::select(HYBAS_ID, date, meanEVI) %>%
-  mutate(year = year(date),
+min_evi_basins <- evi_basins_1 %>%
+  dplyr::select(HYBAS_ID, image_date, mean_EVI) %>%
+  mutate(year = year(image_date),
          HYBAS_ID = as.numeric(HYBAS_ID)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
-  slice_min(meanEVI, n = 1, na_rm = TRUE) %>%
+  slice_min(mean_EVI, n = 1, na_rm = TRUE) %>%
   slice_head(n = 1) |>
-  rename(min_EVI = meanEVI,
-         min_EVI_date = date)
+  rename(min_EVI = mean_EVI,
+         min_EVI_date = image_date)
 
-mean_evi_basins <- evi_basins_2 %>%
-  dplyr::select(HYBAS_ID, date, meanEVI) %>%
-  mutate(year = year(date),
+mean_evi_basins <- evi_basins_1 %>%
+  dplyr::select(HYBAS_ID, image_date, mean_EVI) %>%
+  mutate(year = year(image_date),
          HYBAS_ID = as.numeric(HYBAS_ID)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
-  summarise(mean_EVI = mean(meanEVI, na.rm = T))
+  summarise(mean_EVI = mean(mean_EVI, na.rm = T))
 
-max_evi_basins |> group_by(HYBAS_ID) |>
+max_evi_basins_1 |> group_by(HYBAS_ID) |>
   count() |> filter(n < 24)
 # 8 basins miss a few years of EVI observations
 
 
 # downloading and cleaning the cropland EVI
-cropland_evi_basins_1 <- read_csv(p("basins_evi/02_mean_africover_cropland_evi_basins.csv"))
-cropland_evi_basins_2 <- read_csv(p("basins_evi/03_ESA_cropland_evi.csv"))
+cropland_evi_basins_1 <- read_csv(p("basins_evi/02_mean_africover_cropland_evi_basin.csv"))
 
-max_cropland_evi_basins_1 <- cropland_evi_basins %>%
+cropland_evi_basins_3 <- read_csv(p("basins_evi/03_ESA_cropland_evi.csv"))
+
+max_cropland_evi_basins_1 <- cropland_evi_basins_1 %>%
   mutate(year = year(image_date)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
   slice_max(mean_EVI, n = 1, na_rm = TRUE) %>%
   slice_head(n = 1) |>
-  rename(max_cropland_EVI = mean_EVI,
-         max_cropland_EVI_date = image_date) %>%
+  rename(max_cropland_EVI_africover = mean_EVI,
+         max_cropland_EVI_date_africover = image_date) %>%
   dplyr::select(-basin_id)
 
-max_cropland_evi_basins_2 <- cropland_evi_basins_2 %>%
-  mutate(year = year(date)) %>%
+max_cropland_evi_basins_3 <- cropland_evi_basins_3 %>%
+  mutate(year = year(image_date)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
-  mutate(meanEVI = ifelse(meanEVI == -9999, NA, meanEVI)) %>%
-  slice_max(meanEVI, n = 1, na_rm = TRUE) %>%
+  slice_max(mean_EVI, n = 1, na_rm = TRUE) %>%
   slice_head(n = 1) |>
-  rename(max_cropland_EVI = meanEVI,
-         max_cropland_EVI_date = date) %>%
-  dplyr::select(-`system:index`)
+  rename(max_cropland_EVI_ESA = mean_EVI,
+         max_cropland_EVI_date_ESA = image_date) %>%
+  dplyr::select(-basin_id)
+
 
 # checking whether the two datasets have the same EVI values
-view(left_join(max_cropland_evi_basins_1, max_cropland_evi_basins_2, by = c("HYBAS_ID", "year")))
+max_cropland_evi_basins <- full_join(max_cropland_evi_basins_1, max_cropland_evi_basins_3, by = c("HYBAS_ID", "year"))
 
-min_cropland_evi_basins <- cropland_evi_basins %>%
+
+min_cropland_evi_basins_1 <- cropland_evi_basins_1 %>%
   mutate(year = year(image_date)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
   slice_min(mean_EVI, n = 1, na_rm = TRUE) %>%
   slice_head(n = 1) |>
-  rename(min_cropland_EVI = mean_EVI,
-         min_cropland_EVI_date = image_date) %>%
+  rename(min_cropland_EVI_africover = mean_EVI,
+         min_cropland_EVI_date_africover = image_date) %>%
   dplyr::select(-basin_id)
 
-mean_cropland_evi_basins <- cropland_evi_basins %>%
+mean_cropland_evi_basins <- cropland_evi_basins_1 %>%
   mutate(year = year(image_date)) %>%
   arrange(HYBAS_ID, year) %>%
   group_by(HYBAS_ID, year) %>%
-  summarise(mean_cropland_EVI = mean(mean_EVI, na.rm = T))
+  summarise(mean_cropland_EVI_africover = mean(mean_EVI, na.rm = T))
 
+
+min_cropland_evi_basins_3 <- cropland_evi_basins_3 %>%
+  mutate(year = year(image_date)) %>%
+  arrange(HYBAS_ID, year) %>%
+  group_by(HYBAS_ID, year) %>%
+  slice_min(mean_EVI, n = 1, na_rm = TRUE) %>%
+  slice_head(n = 1) |>
+  rename(min_cropland_EVI_ESA = mean_EVI,
+         min_cropland_EVI_date_ESA = image_date) %>%
+  dplyr::select(-basin_id)
+
+min_cropland_evi_basins <- full_join(min_cropland_evi_basins_3, min_cropland_evi_basins_1)
+
+mean_cropland_evi_basins_1 <- cropland_evi_basins_1 %>%
+  mutate(year = year(image_date)) %>%
+  arrange(HYBAS_ID, year) %>%
+  group_by(HYBAS_ID, year) %>%
+  summarise(mean_cropland_EVI_africover_ESA = mean(mean_EVI, na.rm = T))
+
+mean_cropland_evi_basins_3 <- cropland_evi_basins_3 %>%
+  mutate(year = year(image_date)) %>%
+  arrange(HYBAS_ID, year) %>%
+  group_by(HYBAS_ID, year) %>%
+  summarise(mean_cropland_EVI_africover_ESA = mean(mean_EVI, na.rm = T))
+
+mean_cropland_evi_basins <- full_join(mean_cropland_evi_basins_1, mean_cropland_evi_basins_3)
 
 ## UPDATE
 # joining the data --------------------------------------------------------
 
-basin_evi <- left_join(max_evi_basins, min_evi_basins) |>
+basin_evi <- left_join(max_evi_basins_1, min_evi_basins) |>
   left_join(mean_evi_basins) |>
-  left_join(max_cropland_evi_basins_1) |>
+  left_join(max_cropland_evi_basins) |>
   left_join(min_cropland_evi_basins) |>
   left_join(mean_cropland_evi_basins) |>
   relocate(year, .after = HYBAS_ID)
