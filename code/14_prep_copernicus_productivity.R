4862a13d467bb936a9917c26597d7165eecf89e0
gustavpirich <gustav@pirich.at>
2024-07-27 22:44:43 +0000

    crop productivty data

--- code/14_prep_copernicus_productivity.R
+++ code/14_prep_copernicus_productivity.R
@@ -2,12 +2,24 @@ library(ncdf4)
 library(raster)
 library(sf)
 
-##############################
-#Don't Run IN PROGRESS########
-##############################
-
-
 # Step 1: Read the NetCDF data
+# Define the directory containing the zip files
+zip_directory <- p("copernicus_crop_productivity/2003") # Replace with your directory path
+
+# Define the output directory for unzipped files
+output_directory <- p("copernicus_crop_productivity/2003/")  # Replace with your output directory path
+
+# List all the zip files in the directory
+zip_files <- list.files(zip_directory, pattern = "*.zip", full.names = TRUE)
+
+# Loop through each zip file and unzip it
+for (zip_file in zip_files) {
+  # Unzip the file
+  unzip(zip_file, exdir = output_directory)
+  
+  # Print a message to indicate the progress
+  cat("Unzipped:", zip_file, "\n")
+}
 
 nc_data <- nc_open(p("copernicus_crop_productivity/2004/Maize_TAGP_C3S-glob-agric_2004_1_2004-01-10_dek_CSSF_hist_v1.nc"))
 variable <- ncvar_get(nc_data, "TAGP") # Replace with your variable name
